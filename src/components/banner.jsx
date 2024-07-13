d7983871d3c88a19074c814c511a3bc33a58b9a0
Nasim <nasim.programmer24.com>
2024-07-14 01:15:18 +0600

    again testing

--- src/components/banner.jsx
+++ src/components/banner.jsx
@@ -3,6 +3,7 @@ const banner = () => {
     <div>
       <h1>This banner is made by Nasim</h1>
       <h3>This is a simple banner</h3>
+      <h3>This is simple banner for test.......</h3>
     </div>
   );
 };
