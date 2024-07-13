486ba6cb9b63452b6b763cd0413ab3dc7108c0e1
codeamin11 <legacyofalamin@gmail.com>
2024-07-14 00:38:08 +0600

    add: added a component in app.jsx

---
+++ src/components/Alamin.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Alamin = () => {
+    return (
+        <div>
+            <h2 className="text-3xl">hi this is Alamin
+            </h2>
+        </div>
+    );
+};
+
+export default Alamin;
